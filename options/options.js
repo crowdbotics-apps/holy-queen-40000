f12af35b335b20d2d30228408decb127367a865a
Crowdbotics <team@crowdbotics.com>
2023-04-18 10:20:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-queen-40000",
+  url: "https://holy-queen-40000.botics.co",
+  api: "https://holy-queen-40000.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
